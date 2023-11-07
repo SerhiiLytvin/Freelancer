cdcc8d70557263f69ee1c05fae40a4f569a3dace
SerhiiLytvin <slytvyn20@gmail.com>
2023-11-07 12:48:52 +0200

    radio

--- How it should be done/CheckBoxes and RadioButtons/js/script.js
+++ How it should be done/CheckBoxes and RadioButtons/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+   // CHECK
    $.each($('.checkbox'), function (index, val) {
       if ($(this).find('input').prop('checked') == true) {
          $(this).addClass('active');
@@ -12,6 +13,19 @@ $(document).ready(function () {
       };
       $(this).toggleClass('active');
 
+      return false;
+   })
+   // RADIO
+   $.each($('.radiobuttons__item'), function (index, val) {
+      if ($(this).find('input').prop('checked') == true) {
+         $(this).addClass('active');
+      }
+   });
+   $(document).on('click', '.checkbox', function (event) {
+      $(this).parents('.radiobuttons').find('.radiobuttons_item').removeClass('active');
+      $(this).parents('.radiobuttons').find('.radiobuttons_item input').prop('checked', false);
+      $(this).toggleClass('active');
+      $(this).find('input').props('checked', true);
       return false;
    })
 })
